f1678ca6c45710a7b5afacaf50ff8d6536c040e4
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-23 06:37:10 +0900

    indentation, use jwt

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 const express = require(`express`);
+
 const helmet = require(`helmet`);
 const xss = require(`xss-clean`);
 const mongoSanitize = require(`express-mongo-sanitize`);
 const compression = require(`compression`);
 const cors = require(`cors`);
+
 const passport = require(`passport`);
 const httpStatus = require(`http-status`);
+
 const config = require(`./config/config`);
 const morgan = require(`./config/morgan`);
 const { jwtStrategy } = require(`./config/passport`);
 const { authLimiter } = require(`./middlewares/rateLimiter`);
+
 const routes = require(`./routes/v1`);
+const static = require(`./static/router`);
+
 const { errorConverter, errorHandler } = require(`./middlewares/error`);
 const ApiError = require(`./utils/ApiError`);
 
@@ -44,8 +50,8 @@ if (config.env.cors) {
 }
 
 // jwt authentication
-// app.use(passport.initialize());
-// passport.use(`jwt`, jwtStrategy);
+app.use(passport.initialize());
+passport.use(`jwt`, jwtStrategy);
 
 // limit repeated failed requests to auth endpoints
 if (config.env === `production`) {
@@ -54,6 +60,9 @@ if (config.env === `production`) {
 
 // v1 api routes
 app.use(`/v1`, routes);
+
+// Static pages for testing
+app.use(`/static`, static);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
