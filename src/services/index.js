f2f38281f9ea9465c908c0f510718ad0ee85ebd2
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-22 22:32:40 +0900

    add modules to bundle

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,7 @@
-module.exports.authService = require(`./auth.service`);
-module.exports.emailService = require(`./email.service`);
-module.exports.tokenService = require(`./token.service`);
-module.exports.userService = require(`./user.service`);
+module.exports = {
+  authService: require(`./auth.service`),
+  emailService: require(`./email.service`),
+  tokenService: require(`./token.service`),
+  userService: require(`./user.service`),
+  // potd: require(`./potd.service`),
+};
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,2 +1,5 @@
-module.exports.authValidation = require(`./auth.validation`);
-module.exports.userValidation = require(`./user.validation`);
+module.exports = {
+  authValidation: require(`./auth.validation`),
+  userValidation: require(`./user.validation`),
+  potdValidation: require(`./potd.validation`),
+};
