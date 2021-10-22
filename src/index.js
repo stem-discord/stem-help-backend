0deb13465dd6555e61b61a096010337d25da39af
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-23 06:37:25 +0900

    fix docs url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info(`Connected to MongoDB`);
   server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port} url: http://localhost:${config.port}/docs/v1/`);
+    logger.info(`Listening to port ${config.port} url: http://localhost:${config.port}/v1/docs`);
   });
 });
 
