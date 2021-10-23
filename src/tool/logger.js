2f69ae7c91048a70f63b82e2bbcff94ab534e60f
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-24 07:21:54 +0900

    add fancy formatting to logger

--- src/tool/logger.js
+++ src/tool/logger.js
@@ -18,7 +18,7 @@ const logger = winston.createLogger({
       ? winston.format.colorize()
       : winston.format.uncolorize(),
     winston.format.splat(),
-    winston.format.printf(({ level, message }) => `${level}: ${message}`),
+    winston.format.printf(({ level, message }) => `[🕜${time}] ${level}: ${message}`),
   ),
   transports: [
     new winston.transports.Console({
