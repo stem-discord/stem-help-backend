eb1918035a3220445fbe5b1c20372b1ad5b954df
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-24 15:49:09 +0900

    add env description print command

---
+++ script/envHelp.js
@@ -0,0 +1,26 @@
+const s = require(`../src/config/envVarsSchema`);
+
+// TODO: make this better
+function print(joiKey) {
+  console.log(joiKey.NODE_ENV);
+  for (const v of Object.values(joiKey)) {
+    const flags = v.flags;
+    delete v.flags;
+    for (const [k, vv] of Object.entries(flags)) {
+      v[k] = vv;
+    }
+    if (v.allow) {
+      v.options = `<${v.allow.join(`|`)}>`;
+      delete v.allow;
+    }
+    if (!v.description) {
+      v.description = `No description`;
+    }
+    if (v.presence) {
+      v.default = `required`;
+    }
+  }
+  console.table(joiKey, [`default`, `type`, `options`, `description`]);
+}
+
+print(s.describe().keys);
