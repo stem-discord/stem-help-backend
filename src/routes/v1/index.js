24fca32ccf5e3f4ed3f1a1193fa6a4ce41a6eddf
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-26 03:02:08 +0900

    add routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,9 +4,15 @@ const router = express.Router();
 
 const test = require(`./test`);
 const docs = require(`./docs`);
+const auth = require(`./auth`);
+const oauth = require(`./oauth`);
 
 router.use(`/test`, test);
 
 router.use(`/docs`, docs);
+
+router.use(`/auth`, auth);
+
+router.use(`/oauth`, oauth);
 
 module.exports = router;
