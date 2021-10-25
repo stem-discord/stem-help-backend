92b1d8f2d6e0c0eb8f4de67dcf5ccffb16603a74
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-26 02:10:44 +0900

    finish passport middleware

--- src/tool/passport.js
+++ src/tool/passport.js
@@ -1,16 +1,18 @@
 const { Strategy: JwtStrategy, ExtractJwt } = require(`passport-jwt`);
+
 const config = require(`../config`);
-const { tokenTypes } = require(`../types`);
+const { Token } = require(`../types`);
 const { User } = require(`../models`);
 
 const jwtOptions = {
   secretOrKey: config.jwt.secret,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  algorithms: [`RS256`],
 };
 
 const jwtVerify = async (payload, done) => {
   try {
-    if (payload.type !== tokenTypes.ACCESS) {
+    if (payload.type !== Token.ACCESS) {
       throw new Error(`Invalid token type`);
     }
     const user = await User.findById(payload.sub);
