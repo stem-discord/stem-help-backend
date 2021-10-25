75622605340bc0bd3d67e8a171362af976ac8b5b
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-26 03:02:24 +0900

    discord oauth redirect

--- src/routes/v1/oauth/index.js
+++ src/routes/v1/oauth/index.js
@@ -1,12 +1,27 @@
 const express = require(`express`);
 
+const lib = require(`../../lib`);
+
+const { config } = lib.config;
+const { discord } = lib.util;
+const { urlBuilder } = discord.oauth;
+
+const { ApiError } = lib.util;
+const httpStatus = require(`http-status`);
+
 const router = express.Router();
 
-router.route(`/`)
+
+// returns the OAuth URI
+router.route(`/discord`)
   .get((req, res) => {
-    res.json({
-      message: `yo this works`,
-    });
-  });
+    if (config.DISCORD_OAUTH_URI) {
+      return res.redirect(
+        config.DISCORD_OAUTH_URI,
+      );
+    }
+    throw new ApiError(httpStatus.NOT_FOUND, `DISCORD_OAUTH_URI is not set`, true);
+  },
+  );
 
 module.exports = router;
