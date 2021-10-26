69e9f862642ca777331e6c022b3b5cd23d2e45e6
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-26 18:24:10 +0900

    fix file reader

--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -1,5 +1,6 @@
 const fs = require(`fs`);
+const path = require(`path`);
 
 module.exports = {
-  misconfiguration: fs.readFileSync(`./misconfiguration.ejs`),
+  misconfiguration: fs.readFileSync(path.join(__dirname, `./misconfiguration.ejs`)),
 };
--- src/pages/status/index.js
+++ src/pages/status/index.js
@@ -1,5 +1,6 @@
 const fs = require(`fs`);
+const path = require(`path`);
 
 module.exports = {
-  statistics: fs.readFileSync(`./statistics`),
+  statistics: fs.readFileSync(path.join(__dirname, `./statistics.ejs`)),
 };
