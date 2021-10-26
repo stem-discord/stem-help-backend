c046a5ad388c723cecd00852542cdd2fd2a66b92
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-26 18:23:49 +0900

    disable global jwt authentication

--- src/app.js
+++ src/app.js
@@ -51,10 +51,6 @@ if (config.env.cors) {
   app.options(`*`, cors());
 }
 
-// jwt authentication
-app.use(passport.initialize());
-passport.use(`jwt`, jwtStrategy);
-
 // limit repeated failed requests to auth endpoints
 if (config.env === `production`) {
   app.use(`/v1/auth`, authLimiter);
