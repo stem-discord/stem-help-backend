c28a2ee59bd6b8cb78652fbcac78d0d1b5155146
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-27 07:48:02 +0900

    allow api route

--- src/config/process.js
+++ src/config/process.js
@@ -7,10 +7,7 @@ module.exports = function (config) {
   if (config.NODE_ENV === `production` && config.STATIC_SERVER) {
     throw new Error(`STATIC_SERVER must be set to false if NODE_ENV is production`);
   }
-  if (config.staticServer || config.staticServerPort || config.staticServerApiURL) {
-    throw new Error(`staticServer, staticServerPort, and staticServerApiURL are in development`);
+  if (config.staticServer || config.staticServerPort) {
+    throw new Error(`staticServer, staticServerPort are in development`);
   }
-// STATIC_SERVER: Joi.st
-// STATIC_SERVER_PORT: J
-// STATIC_SERVER_API_URL
 };
