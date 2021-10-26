1ed6c6bb134fd9287d31663d47ea5fe85b0695e2
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-26 15:51:23 +0900

    add discord auth

--- src/routes/v1/auth/index.js
+++ src/routes/v1/auth/index.js
@@ -1,5 +1,24 @@
+const httpStatus = require(`http-status`);
+
 const lib = require(`../../lib`);
 
+const config = lib.config;
+const { discord } = lib.util;
+const { buildUri } = discord.oauth;
+const { ApiError } = lib.util;
+
 const router = lib.Router();
+
+// == DISCORD ==
+// returns the OAuth URI
+router.route(`/discord`)
+  .get((req, res) => {
+    const uri = config.discord.OAuthUri;
+    if (uri) {
+      return res.redirect(uri);
+    }
+    throw new ApiError(httpStatus.NOT_FOUND, `DISCORD_OAUTH_URI is not set`, true);
+  },
+  );
 
 module.exports = router;
