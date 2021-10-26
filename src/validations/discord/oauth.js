4c1a384ff25c454ce2ddb3dd4814b536dd716ebe
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-26 15:03:13 +0900

    add return code validation

---
+++ src/validations/discord/oauth.js
@@ -0,0 +1,20 @@
+const Joi = require(`joi`);
+
+const token = (value, helpers) => {
+  // TODO: I'm not sure if this is the correct regex, but its from observation
+  if (!value.match(/^[\da-zA-Z_-]+$/)) {
+    return helpers.message(`"{{#label}}" must be a valid token`);
+  }
+  return value;
+};
+
+
+const redirectUri = {
+  query: Joi.object().keys({
+    code: Joi.string().custom(token).required(),
+  }),
+};
+
+module.exports = {
+  redirectUri,
+};
