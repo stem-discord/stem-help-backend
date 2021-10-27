683869f6258c64e8609826578b1891405edb1a7a
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-27 11:59:19 +0900

    implement oauth

--- src/service/user.js
+++ src/service/user.js
@@ -8,6 +8,16 @@ const getUserById = async (id) => {
   return User.findById(id);
 };
 
+const createUser = User.create;
+
+const discord = {
+  getUserById: async (id) => {
+    return User.findOne({ "discord.id": id });
+  },
+};
+
 module.exports = {
   getUserById,
+  createUser,
+  discord,
 };
