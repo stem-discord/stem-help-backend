b311a5b885de6137e47f0f2ef5f27a8ad1e8ecef
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-29 03:05:37 +0900

    include connection types

---
+++ src/types/ConnectionEvent/index.js
@@ -0,0 +1,11 @@
+export default {
+  connected: `connected`,
+  disconnected: `disconnected`,
+  connecting: `connecting`,
+  disconnecting: `disconnecting`,
+  open: `open`,
+  close: `close`,
+  reconnected: `reconnected`,
+  error: `error`,
+  fullsetup: `fullsetup`,
+};
--- src/types/index.js
+++ src/types/index.js
@@ -1,2 +1,4 @@
 export { default as Token } from "./Token";
 export { default as Role } from "./Role";
+export { default as ConnectionEvent } from "./ConnectionEvent";
+export { default as ConnectionState } from "./ConnectionState";
