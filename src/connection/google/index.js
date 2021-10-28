4996a0727cffc1a6d5392e1e0b9eb84cee6dda68
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-29 03:06:14 +0900

    .

--- src/connection/google/index.js
+++ src/connection/google/index.js
@@ -1,1 +1,10 @@
-import config from "../../config";
+// import fetch from 'node-fetch';
+
+// import config from "../../config";
+// import { Interface, NullInterface } from "../interface";
+
+// const googleInterface = new Interface({
+//   init:
+// })
+
+// export { googleInterface };
