3890b68c8284a92b8b67f97bd07061a48b7ddd90
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-29 14:40:45 +0900

    strict api constructor scheme

--- src/util/ApiError.js
+++ src/util/ApiError.js
@@ -1,5 +1,10 @@
+import httpStatus from "http-status";
+
 class ApiError extends Error {
   constructor(statusCode, message, isOperational = true, stack = ``) {
+    if (httpStatus[statusCode] === undefined) {
+      throw new Error(`Status code must be provided for ApiErrors`);
+    }
     super(message);
     this.statusCode = statusCode;
     this.isOperational = isOperational;
