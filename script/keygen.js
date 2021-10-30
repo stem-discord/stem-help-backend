2247cfa90833632a48aea770296ac53d39c299fd
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-30 19:03:49 +0900

    improve key generation script

--- script/keygen.js
+++ script/keygen.js
@@ -13,6 +13,15 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const pubpath = path.join(__dirname, `../env/id_rsa_pub.pem`),
+  privpath = path.join(__dirname, `../env/id_rsa_priv.pem`);
+
+// check if files already exist since it would be terrible to overwrite them
+if (fs.existsSync(pubpath) || fs.existsSync(privpath)) {
+  throw new Error(`Files already exist, please remove them before generating new keys`);
+}
+
 function genKeyPair() {
 
   // Generates an object where the keys are stored in properties `privateKey` and `publicKey`
@@ -29,11 +38,12 @@ function genKeyPair() {
   });
 
   // Create the public key file
-  fs.writeFileSync(path.join(__dirname, `../env/id_rsa_pub.pem`), keyPair.publicKey);
+  fs.writeFileSync(pubpath, keyPair.publicKey);
 
   // Create the private key file
-  fs.writeFileSync(path.join(__dirname, `../env/id_rsa_priv.pem`), keyPair.privateKey);
+  fs.writeFileSync(privpath, keyPair.privateKey);
 
+  console.log(`generated keypair`);
 }
 
 // Generate the keypair
