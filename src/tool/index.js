611ec857fe71263682ccd4f89177989e13961491
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-30 20:43:07 +0900

    remove passport from tools

--- src/tool/index.js
+++ src/tool/index.js
@@ -1,6 +1,5 @@
 export { logger, Logger } from "./logger.js";
 
-export * as passport from "./passport";
 export * as morgan from "./morgan.js";
 export * as roleManager from "./roleManager.js";
 export * as token from "./token.js";
