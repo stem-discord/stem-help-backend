b1de806c5a1b4e95fa35fb6cd2a9e71290f09ebb
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-30 20:43:19 +0900

    export other models as well

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,13 +1,14 @@
-import { User as UserSchema } from "../models";
+import * as models from "../models";
 
 import * as connection from "../connection";
 import { RequireProxy } from "./RequireProxy";
 
 const mongo = new RequireProxy(() => connection.mongo.connection.isOperational(), {
-  User: connection.mongo.model(`User`, UserSchema),
+  User: connection.mongo.model(`User`, models.User),
+  Session: connection.mongo.model(`Session`, models.Session),
+  Token: connection.mongo.model(`Token`, models.Token),
+  Potd: connection.mongo.model(`Potd`, models.Potd),
 });
-
-// console.log(connection.discord.client);
 
 const stem = new RequireProxy(() => connection.discord.connection.isOperational(), {
   get generalChannel() { return connection.discord.client.chanels.fetch(`839399426643591188`); },
