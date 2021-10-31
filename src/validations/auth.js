7f701704bd5ccd45005b5b646e262cafa7d32cf6
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-31 18:25:05 +0900

    update validations

--- src/validations/auth.js
+++ src/validations/auth.js
@@ -3,49 +3,16 @@ import { password } from "./custom.js";
 
 const register = {
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    username: Joi.string().required(),
     password: Joi.string().required().custom(password),
-    name: Joi.string().required(),
   }),
 };
 
 const login = {
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    username: Joi.string().required(),
     password: Joi.string().required(),
   }),
 };
 
-const logout = {
-  body: Joi.object().keys({
-    refreshToken: Joi.string().required(),
-  }),
-};
-
-const refreshTokens = {
-  body: Joi.object().keys({
-    refreshToken: Joi.string().required(),
-  }),
-};
-
-const forgotPassword = {
-  body: Joi.object().keys({
-    email: Joi.string().email().required(),
-  }),
-};
-
-const resetPassword = {
-  query: Joi.object().keys({
-    token: Joi.string().required(),
-  }),
-  body: Joi.object().keys({
-    password: Joi.string().required().custom(password),
-  }),
-};
-
-const verifyEmail = {
-  query: Joi.object().keys({
-    token: Joi.string().required(),
-  }),
-};
-export { register, login, logout, refreshTokens, forgotPassword, resetPassword, verifyEmail };
+export { login, register };
