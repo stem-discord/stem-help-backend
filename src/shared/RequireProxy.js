83312037a08aaf0cf1c5fa81800fd5fcf57db500
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-31 17:50:12 +0900

    fix this proxy

--- src/shared/RequireProxy.js
+++ src/shared/RequireProxy.js
@@ -3,7 +3,7 @@ import httpStatus from "http-status";
 import { ApiError  } from "../util";
 
 function RequireProxy(validator, obj) {
-  this.cache = {};
+  const cache = {};
   const stack = new Error().stack;
   return new Proxy(obj, {
     get: function (target, name) {
@@ -11,21 +11,18 @@ function RequireProxy(validator, obj) {
         throw new Error(`${name} is not a property`, stack);
       }
       // eslint-disable-next-line no-async-promise-executor
-      return new Promise(async (resolve, reject) => {
-        try {
-          if (!await validator()) {
-            reject(new ApiError(httpStatus.SERVICE_UNAVAILABLE, `Cannot access property ${name} because it is unavailable`));
-          }
-          if (!Object.prototype.hasOwnProperty.call(this.cache, name)) {
-            this.cache[name] = await target[name];
-          }
-          resolve(this.cache[name]);
-        } catch (e) {
-          reject(new ApiError(httpStatus.INTERNAL_SERVER_ERROR, `Unknown error`));
-          e.stack += `\n\nInitialization stack` + stack.substring(stack.indexOf(`\n`, stack.indexOf(`\n`) + 1));
-          throw e;
+      try {
+        if (!validator()) {
+          throw new ApiError(httpStatus.SERVICE_UNAVAILABLE, `Cannot access property ${name} because it is unavailable`);
         }
-      });
+        if (!Object.prototype.hasOwnProperty.call(cache, name)) {
+          cache[name] = target[name];
+        }
+        return cache[name];
+      } catch (e) {
+        e.stack += `\n\nInitialization stack` + stack.substring(stack.indexOf(`\n`, stack.indexOf(`\n`) + 1));
+        throw e;
+      }
     },
   });
 }
