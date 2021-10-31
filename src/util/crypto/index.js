15a2e167f3697b196dff02147fe3093dfbe811e8
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-31 17:49:35 +0900

    fix signature

--- src/util/crypto/index.js
+++ src/util/crypto/index.js
@@ -29,9 +29,9 @@ function validatePassword(password, hash, salt) {
 function generatePassword(password) {
 
   const salt = crypto.randomBytes(32).toString(`hex`);
-  const genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, `sha512`).toString(`hex`);
+  const hash = crypto.pbkdf2Sync(password, salt, 10000, 64, `sha512`).toString(`hex`);
 
-  return [salt, genHash];
+  return { salt, hash };
 }
 
 export { generatePassword, validatePassword };
