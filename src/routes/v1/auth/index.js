a602b26e208139da9cd41d808ebcacbaa8d353b8
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-31 16:52:32 +0900

    implement register and login

--- src/routes/v1/auth/index.js
+++ src/routes/v1/auth/index.js
@@ -4,9 +4,10 @@ import * as lib from "../../lib";
 
 const config = lib.config;
 const { discord } = lib.util;
+const service = lib.service;
 
 const { buildUri } = discord.oauth;
-const { ApiError } = lib.util;
+const { ApiError, catchAsync } = lib.util;
 
 const router = lib.Router();
 
@@ -24,5 +25,52 @@ router.route(`/discord`)
 router.route(`/refresh`)
   .get((req, res) => {
   });
+
+// standard password and
+router.route(`/login`)
+  .post(catchAsync(async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ApiError(httpStatus.BAD_REQUEST, `username and password are required`);
+    }
+
+    const user = await service.user.getBy.username(username);
+    if (!user) {
+      throw new ApiError(httpStatus.NOT_FOUND, `user not found`);
+    }
+
+    const isValid = await service.user.validatePassword(user, password);
+    if (!isValid) {
+      throw new ApiError(httpStatus.UNAUTHORIZED, `invalid password`);
+    }
+
+    const ip = req.headers[`x-forwarded-for`] || req.socket.remoteAddress;
+
+    const tokenPair = await service.token.createSession(user, `session at ${ip}`);
+
+    res.json(tokenPair);
+
+  }));
+
+router.route(`/register`)
+  .post(catchAsync(async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ApiError(httpStatus.BAD_REQUEST, `username and password are required`);
+    }
+
+    const user = await service.user.getBy.username(username);
+    if (user) {
+      throw new ApiError(httpStatus.CONFLICT, `username already exists`);
+    }
+
+    const newUser = await service.user.create(username, password);
+
+    const ip = req.headers[`x-forwarded-for`] || req.socket.remoteAddress;
+
+    const tokenPair = await service.token.createSession(newUser, `session at ${ip}`);
+
+    res.json(tokenPair);
+  }));
 
 export default router;
