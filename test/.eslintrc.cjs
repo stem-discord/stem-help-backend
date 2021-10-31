9472d7dee6e80ff38869bcaebb1f8e5d8c0f89e2
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-31 16:07:52 +0900

    override

--- test/.eslintrc.cjs
+++ test/.eslintrc.cjs
@@ -2,7 +2,9 @@ module.exports = {
   env: {
     mocha: true,
   },
-  globals: {
-    expect: true,
+  globals: {},
+  rules: {
+    'no-unused-expressions': `off`,
+    'no-unused-vars': `off`,
   },
 };
