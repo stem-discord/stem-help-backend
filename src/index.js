6636a379c2815c4ffda7d45f88326e9517191273
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-01 11:26:10 +0900

    split to exports and server so it is easier for importing seprate modules

--- src/index.js
+++ src/index.js
@@ -1,78 +1,16 @@
-import app from "./app.js";
-import config from "./config";
-import { Logger } from "./tool";
-import { git } from "./util";
-const { status } = git;
-
-const logger = new Logger(`Index`);
-
-logger.info(`Finished importing modules`);
-
-// Log
-(async () => {
-  const o = {
-    branch: await status.getBranch(),
-    commit: await status.getLastCommit(),
-  };
-  const diff = {
-    head: await status.getHeadDiff(),
-    unstaged: await status.getUnstagedDiff(),
-  };
-  const l = [`Git Info: \nBranch ${o.branch}#${o.commit}`];
-  const diffs = [
-    [`Unstaged diff`, diff.unstaged],
-    [`Head diff`, diff.head],
-  ];
-  for (const [title, val] of diffs) {
-    if (val) {
-      l.push(`[${title}]\n${val}`);
-    }
-  }
-  logger.info(l.join(`\n\n`));
-  if (l.length <= 1) {
-    logger.info(`working directory is in sync`);
-  }
-  logger.info(`Node version: ${process.version}${process.arch}-${process.platform} mode: ${config.env}`);
-})();
-
-const apiServer = app.listen(config.port, () => {
-  logger.info(`App is on '${config.env}' mode`);
-  logger.info(`Listening to port ${config.port} - http://localhost:${config.port}/v1/docs`);
-});
-
-let staticServer;
-
-const exitHandler = () => {
-  process.emit(`SIGTERM`, 1);
-};
-
-const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
-};
-
-process.on(`uncaughtException`, unexpectedErrorHandler);
-process.on(`unhandledRejection`, unexpectedErrorHandler);
-
-process.on(`SIGTERM`, async (code = 0) => {
-  try {
-    logger.info(`SIGTERM signal received`);
-
-    const funcs = [
-      apiServer.close,
-      staticServer?.close,
-    ];
-    const promises = [];
-    for (const f of funcs) {
-      if (f) {
-        let p = f();
-        promises.push(p);
-      }
-    }
-    await Promise.all(promises);
-    logger.info(`released resources`);
-    process.exit(code);
-  } catch {
-    // do nothing. let the app crash
-  }
-});
+export { default as tool } from "./tool";
+export { default as util } from "./util";
+export { default as config } from "./config";
+export { default as app } from "./app";
+export { default as auth } from './auth';
+export { default as connection } from "./connection";
+export { default as docs } from "./docs";
+export { default as middlewares } from "./middlewares";
+export { default as models } from "./models";
+export { default as pages } from "./pages";
+export { default as routes } from "./routes";
+export { default as service } from "./service";
+export { default as shared } from "./shared";
+export { default as static } from "./static";
+export { default as types } from "./types";
+export { default as validations } from "./validations";
