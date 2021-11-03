94da0a69512b273a55be8c992f375f8056a82dcc
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-03 14:10:39 +0900

    create temporary role manager file

--- src/tool/roleManager.js
+++ src/tool/roleManager.js
@@ -1,3 +1,21 @@
 import { Role } from "../types";
 
-export default {};
+/**
+ * Loads user from database roles
+ */
+class User {
+  constructor(roles) {
+    this.roles = roles;
+  }
+
+  can(role) {
+    if (Role[role] === undefined) {
+      throw new Error(`Role ${role} does not exist`);
+    }
+    return this.roles.includes(role);
+  }
+}
+
+// TODO: move this entire section to the model of userSchema including the calculating logic for primitives
+
+export { User };
