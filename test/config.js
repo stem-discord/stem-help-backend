ab1459cad1f56d3fd48cda3ee02196b7487aa260
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-25 23:19:57 +0900

    add config parser

---
+++ test/config.js
@@ -0,0 +1,42 @@
+// Config loader
+
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from 'url';
+
+import yargs from "yargs";
+import dotenv from "dotenv";
+import Joi from "joi";
+
+import print from "../script/util/printJoiSchema.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const argv = yargs.option(`file`, {
+  alias: `f`,
+  describe: `config file to read`,
+  type: `string`,
+  default: `.env`,
+}).argv;
+
+const envPath = path.join(__dirname, argv.f);
+
+const env = dotenv.parse(fs.readFileSync(envPath));
+
+const schema = Joi.object().keys({
+  API_URL: Joi.string()
+    .description(`API URL to connect to (client)`).pattern(/^http/),
+});
+
+const { value: vars, error } = schema
+  .prefs({ errors: { label: `key` } })
+  .validate(env);
+
+if(error) {
+  /* eslint-disable no-console */
+  console.log(`Invalid schema! Please refer to the table above. File: ${envPath}`);
+  print(schema);
+  throw error;
+}
+
+export default env;
