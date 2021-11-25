26b85abb6ce9c085c7cde89692fb690e597ff6cf
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-26 06:23:28 +0900

    use lowercase

--- src/shared/index.js
+++ src/shared/index.js
@@ -8,7 +8,7 @@ const mongo = new RequireProxy(() => connection.mongo.connection.isOperational()
   get Session() { return connection.mongo.model(`Session`, models.Session); },
   get Token() { return connection.mongo.model(`Token`, models.Token); },
   get Potd() { return connection.mongo.model(`Potd`, models.Potd); },
-  get Data() { return connection.mongo.model(`Data`, models.Data); },
+  get Data() { return connection.mongo.model(`Data`, models.Data, `data`); },
 });
 
 const stem = new RequireProxy(() => connection.discord.connection.isOperational(), {
