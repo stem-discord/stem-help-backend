a970fb8fcce46ef238a254760d0b70b4ae27548e
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-26 04:06:55 +0900

    add group field type

--- src/types/index.js
+++ src/types/index.js
@@ -1,4 +1,5 @@
 export { default as Token } from "./Token";
 export { default as Role } from "./Role";
+export { default as Group } from "./Group";
 export { default as ConnectionEvent } from "./ConnectionEvent";
 export { default as ConnectionState } from "./ConnectionState";
