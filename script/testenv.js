8894a25a719ec7bb559569f7eee11872343c8665
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-26 10:05:24 +0900

    fix ci

--- script/testenv.js
+++ script/testenv.js
@@ -8,6 +8,7 @@ const file = path.join(__dirname, `../test/.env`);
 if (!fs.existsSync(file)) {
   // TODO softcode this
   // If you have a lot of time, just rewrite the entire setup thing
+  if (!process.env.PORT) throw new Error(`Pass in a PORT environment variable!`);
   fs.writeFileSync(file, `API_URL=http://localhost:${process.env.PORT}/v1`);
   console.log(`./test/.env file did not exist. generated one`);
 }
