8015f7426e7af0379863158fa12ba9c9ede30eed
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-28 06:17:48 +0900

    finish this function

--- src/service/discord.js
+++ src/service/discord.js
@@ -1,28 +1,85 @@
-import { discord } from "../shared";
+import SortedArray from "sorted-array";
+import stringSimilarity from "string-similarity";
+// import Discord from "discord.js";
+
+import * as shared from "../shared";
+import { DSA, normalize } from "../util";
+
+const similarityScore = Symbol(`similarityScore. The lower the score, the greater the similarity`);
+
+const EMPTY_USER = {
+  [similarityScore]: Infinity,
+};
 
 /**
- * @return {Promise<import("discord.js").User[]>}
+ * @param {string} [anything] If it only has numbers and lenght is 16-19, it will attempt to see if the ID exists.
+ * If this fails, it treats the number sequence as a username
+ * if it has #, the front part will be interpreted as a username, and the second part will be interpreted as tag
+ * the username or nickname (nickname higher priority by factor of 2) will be added to the similarity score if tag is present.
+ * @param {Object} [options] `options.limit` maximum number of users to return. defaults to 10
+ * @return {import("discord.js").User[]} Adds an additional property `nickname` if the user is in stem.
  */
-export async function userResolveAnything(anything, { limit = 10 }) {
-  await discord.client.users.fetch();
-  // let count = 0;
-  const res = [];
-
-  for (let user of discord.client.users.cache.values()) {
-    // If we have the maximum number of users, stop.
-    if (res.length === limit) {
-      break;
-    }
-
-    const stemMember = discord.stem.members.resolve(user);
-    if (stemMember) {
-      res.push(stemMember, [
-        `username`,
-        `tag`,
-        `discriminator`,
-        `createdTimestamp`,
-      ]);
+function userResolveAnything(anything, { limit = 10 } = {}) {
+  const _shared = Object.getPrototypeOf(this) === null ? shared : this;
+  const client = _shared.discord.client;
+  const guild = _shared.discord.stem.guild;
+  // ID match
+  if (anything.match(/^\d{16,19}$/)) {
+    let user = client.user.cache.get(anything);
+    if (user) {
+      return [user];
     }
   }
-  return res;
+
+  const userArray = new SortedArray.comparing(similarityScore, Array(limit).fill(EMPTY_USER));
+
+  const insert = obj => {
+    userArray.insert(obj);
+    userArray.array.pop();
+  };
+
+  const values = client.users.cache.values();
+
+  const lastIndexOfPound = anything.lastIndexOf(`#`);
+
+  let targetUsername, targetDiscriminator;
+
+  if (lastIndexOfPound > 0) {
+    targetUsername = anything.substring(0, lastIndexOfPound);
+    targetDiscriminator = anything.substring(lastIndexOfPound + 1);
+  }
+
+  for (const user of values) {
+    let score = 0;
+    let { username, discriminator } = user;
+    username = normalize(username);
+    let nickname = guild.members.resolve(user)?.nickname;
+    if (nickname) {
+      user.nickname = nickname;
+      const ss = 2 * stringSimilarity.compareTwoStrings(targetUsername, nickname);
+      score = ss > score ? ss : score;
+    }
+    if (username) {
+      const ss = stringSimilarity.compareTwoStrings(targetUsername, username);
+      score = ss > score ? ss : score;
+    }
+    if (discriminator) {
+      score += 0.2 * DSA.lcs.LCSFromStart(targetDiscriminator, discriminator);
+    }
+    user[similarityScore] = -1 * score;
+    insert(user);
+  }
+
+  const arr = [...userArray.array];
+
+  // splice to find non-EMPTY_USER
+  const idx = arr.findIndex(obj => obj === EMPTY_USER);
+
+  if (idx > 0) {
+    arr.splice(idx);
+  }
+
+  return arr;
 }
+
+export { userResolveAnything };
