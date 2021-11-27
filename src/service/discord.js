cb4e3851fb1afd1f9f60fe35f279fa690eaad4ac
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-28 01:04:16 +0900

    fix some module import/exports

--- src/service/discord.js
+++ src/service/discord.js
@@ -1,20 +1,20 @@
-import shared from "../shared";
+import { discord } from "../shared";
 
 /**
  * @return {Promise<import("discord.js").User[]>}
  */
 export async function userResolveAnything(anything, { limit = 10 }) {
-  await shared.discord.client.users.fetch();
+  await discord.client.users.fetch();
   // let count = 0;
   const res = [];
 
-  for (let user of shared.discord.client.users.cache.values()) {
+  for (let user of discord.client.users.cache.values()) {
     // If we have the maximum number of users, stop.
     if (res.length === limit) {
       break;
     }
 
-    const stemMember = shared.discord.stem.members.resolve(user);
+    const stemMember = discord.stem.members.resolve(user);
     if (stemMember) {
       res.push(stemMember, [
         `username`,
--- src/service/index.js
+++ src/service/index.js
@@ -1,3 +1,4 @@
 export * as user from "./user.js";
 export * as swagger from "./swagger.js";
 export * as token from "./token.js";
+export * as discord from "./discord.js";
