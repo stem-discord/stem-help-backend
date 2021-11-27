18cfb8b7fa202e168b678605336a29454226e36f
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-28 06:17:28 +0900

    add DSA test spec

--- test/unit/test.spec.js
+++ test/unit/test.spec.js
@@ -2,10 +2,26 @@ import * as util from "../../src/util";
 
 import { expect, assert } from "chai";
 
-const { pick } = util;
+const { pick, DSA } = util;
 
 describe(`pick.js`, function () {
   it(`should work`, function () {
     expect(pick({ a: 1, b: 2, c: 3 }, [`a`, `b`])).to.deep.equal({ a: 1, b: 2 });
+  });
+});
+
+describe(`DSA`, function () {
+  describe(`lcs.js`, function () {
+    const lcs = DSA.lcs;
+    it(`LCSFromStart`, function () {
+      const L = lcs.LCSFromStart;
+
+      expect(L(`a`)).to.equal(1);
+      expect(L(`ab`)).to.equal(2);
+      expect(L(`ab`, ``)).to.equal(0);
+      expect(L(`ab`, `abcde`)).to.equal(2);
+      expect(L(`ab`, `bcdefa`)).to.equal(0);
+      expect(L(`ab`, `abc`, `abcde`)).to.equal(2);
+    });
   });
 });
