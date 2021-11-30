871ef0c42efb0ac8258cba68ca2ecd91ba0e7a41
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-30 14:38:03 +0900

    server test for mock

---
+++ test/server/main.spec.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch';
+import { nock } from "../mock";
+
+describe(`discord api`, function () {
+  it(`should return 404 (mock)`, async function() {
+    nock(`discord.com`);
+    const res = await fetch(`https://discord.com/api/v9/`)
+      .then(v => v.json());
+    expect(res).to.have.property(`message`);
+    expect(res).to.have.property(`custom`);
+  });
+
+  it(`should return 404 (real)`, async function() {
+    this.skip();
+    const res = await fetch(`https://discord.com/api/v9/`)
+      .then(v => v.json());
+    expect(res).to.have.property(`message`);
+    expect(res).to.not.have.property(`custom`);
+  });
+});
