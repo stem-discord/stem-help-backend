540d047f676eacc29b8e4fc49200731ccb03aa72
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-30 14:41:45 +0900

    allow unmocked

--- test/mock/discord.com/index.js
+++ test/mock/discord.com/index.js
@@ -1,6 +1,6 @@
 import nock from "nock";
 
-const interceptor = nock(`https://discord.com/api/v9`).persist(true) .get(`/`);
+const interceptor = nock(`https://discord.com/api/v9`, { allowUnmocked: true }).persist(true).get(`/`);
 
 const scope = interceptor.reply(404, {
   message: `404: Not Found`, code: 0, custom: `ʕ•́ᴥ•̀ʔっ`,
