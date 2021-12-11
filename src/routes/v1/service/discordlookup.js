236f810605c9ceaea4ff90bb06016d3a92f22157
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-12 04:53:34 +0900

    update lookup service

--- src/routes/v1/service/discordlookup.js
+++ src/routes/v1/service/discordlookup.js
@@ -8,10 +8,27 @@ const config = lib.config;
 
 const router = lib.Router();
 
+function lookup(q) {
+  const users = lib.service.discord.userResolveAnything(q);
+
+  return users.map(v => {
+    const u = pick(v, [`id`, `username`, `discriminator`, `avatar`]);
+    u.nickname = u.member?.nickname;
+    return u;
+  });
+}
+
 router.get(`/:query`, (req, res) => {
   const query = req.params.query;
-  const users = lib.service.discord.userResolveAnything(query);
-  res.json(users.map(v => pick(v, [`id`, `username`, `discriminator`, `avatar`])));
+  res.json(lookup(query));
+});
+
+router.get(`/`, (req, res) => {
+  const query = req.query.query;
+  if (!query) {
+    throw new ApiError(httpStatus.BAD_REQUEST, `Missing query`);
+  }
+  res.json(lookup(query));
 });
 
 export default router;
