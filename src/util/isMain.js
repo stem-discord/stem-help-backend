363fd7b1ddc26569d527064fde08371ac4235ec9
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-12 19:49:57 +0900

    add error

--- src/util/isMain.js
+++ src/util/isMain.js
@@ -1,4 +1,5 @@
 import { fileURLToPath } from "url";
 export default function(meta) {
+  if (!meta?.url) throw new Error(`.url is required`);
   return process.argv[1] === fileURLToPath(meta.url);
 }
