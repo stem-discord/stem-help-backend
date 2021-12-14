d10a774c70a13ffac1448faa10169131332dbf4f
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-14 23:30:50 +0900

    update module connection interface

--- src/connection/index.js
+++ src/connection/index.js
@@ -4,6 +4,8 @@ import * as discord from "./discord/index.js";
 
 import { Logger } from "../tool/index.js";
 import { async } from "../util/index.js";
+import { ConnectionState } from "../types/index.js";
+
 const { sleep } = async;
 
 const logger = new Logger(`ConnectionManager`);
@@ -22,6 +24,28 @@ for (const name of Object.keys(modules)) {
 
 logger.info(`Registered modules: ${moduleNames.join(`, `)}`);
 
+function connDesc(connection) {
+  // The connection is configured
+  if (!connection.null) {
+    // configured and operational
+    if (connection.isOperational()) {
+      return `✔️  ${connection.state}`;
+    }
+    // not operational, for two reasons
+    else {
+      // waiting for opening
+      if ([ConnectionState.UNINITIALIZED, ConnectionState.CONNECTING].includes(connection.state)) {
+        return `⏳ ${connection.state}`;
+      } else if ([ConnectionState.DISCONNECTED, ConnectionState.DISCONNECTING].includes(connection.state)) {
+        return `⚠️ ${connection.state} ${connection.rejectReason}`;
+      }
+    }
+  } else {
+    return `✖️  Not configured`;
+  }
+  return `❔ Unknown status ${connection.state}`;
+}
+
 const openConnections = (async (selection) => {
   selection ??= Object.keys(modules);
   if (!Array.isArray(selection)) {
@@ -33,7 +57,7 @@ const openConnections = (async (selection) => {
   const reports = [];
   const WIDTH = 20;
   for (const connection of connections) {
-    reports.push(`➣ [${connection.name}]`.padEnd(WIDTH) + (!connection.null ? `: ${connection.isOperational() ? `✔️ ` : `❌ `} ${connection.state}` : `⚠️  ${connection.rejectReason}`));
+    reports.push(`➣ [${connection.name}]`.padEnd(WIDTH) + connDesc(connection));
   }
   logger.info(
     `== Open Connection Report ==\n` +
