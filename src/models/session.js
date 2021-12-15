c8ce4dd6cc7ed56f5153602b0dd361b8697470f4
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 00:04:18 +0900

    disable session feature and go for a straightforward auth scheme

--- src/models/session.js
+++ src/models/session.js
@@ -1,16 +1,19 @@
-import { default as mongoose } from "mongoose";
+/**
+ * TEMPORARILY DISABLED FILE
+ */
+// import { default as mongoose } from "mongoose";
 
-import Token from "./token.js";
+// import Token from "./token.js";
 
-const sessionSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    default: () => `Session ` + `${Math.random()}`.substring(2),
-  },
-  active: {
-    type: Token,
-  },
-});
+// const sessionSchema = mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     default: () => `Session ` + `${Math.random()}`.substring(2),
+//   },
+//   active: {
+//     type: Token,
+//   },
+// });
 
-export default sessionSchema;
+export default null;
