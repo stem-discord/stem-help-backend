ae66f1745d7e62ef62b7e36795720814658950e1
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 23:53:47 +0900

    add mongo tests

---
+++ test/service/mongo.spec.js
@@ -0,0 +1,29 @@
+import shared from "../../src/shared/index.js";
+
+import { mongo, openConnections } from "../../src/connection/index.js";
+
+import { user } from "../../src/service/index.js";
+
+describe(`mongo`, function() {
+  describe(`real test`, function() {
+    before(async function () {
+      this.timeout(1000);
+      await openConnections([`mongo`]);
+    });
+    beforeEach(function () {
+      this.needs(mongo);
+    });
+    it(`should find one user`, async function() {
+      const user = await shared.mongo.User.findOne({
+        username: `johndoe`,
+      });
+
+      expect(user).to.be.an(`object`);
+    });
+    it(`should find one user using getBy`, async function() {
+      const u = await user.getBy.username(`johndoe`);
+
+      expect(u).to.be.an(`object`);
+    });
+  });
+});
