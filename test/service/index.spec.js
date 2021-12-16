256402ef332aaf459787db49b18b6fcda2065355
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 23:53:54 +0900

    add a before

--- test/service/index.spec.js
+++ test/service/index.spec.js
@@ -1,9 +1,13 @@
-import { generate as genHtml} from "../../src/service/generatePngFromHtml.js";
-import { generate as genBanner} from "../../src/service/generatePngBanner.js";
+import { generate as genHtml } from "../../src/service/generatePngFromHtml.js";
+import { generate as genBanner } from "../../src/service/generatePngBanner.js";
 
 import fs from "fs";
 
 describe(`generatePngFromHtml`, function() {
+  before(`prime the browser`, async function () {
+    this.timeout(10 * 1000);
+    await genHtml(``);
+  });
   it(`should generate png from html`, async function() {
     const text = `Hello`;
 
