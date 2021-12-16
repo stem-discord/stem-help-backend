c0d624655f62e656e694a7890910b3bf3d4f0848
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 23:54:44 +0900

    rewrite auth

--- src/routes/v1/auth/index.js
+++ src/routes/v1/auth/index.js
@@ -1,38 +1,60 @@
 import httpStatus from "http-status";
+import passport from "passport";
 
 import * as lib from "../../lib/index.js";
 
+const { jwtRefreshStrategy } = lib.auth.jwt;
+
 const config = lib.config;
-const { discord } = lib.util;
+// const { discord } = lib.util;
 const services = lib.service;
 
-const { buildUri } = discord.oauth;
+// const { buildUri } = discord.oauth;
 const { ApiError, catchAsync } = lib.util;
-const { Validate } = lib.middlewares;
+// const { Validate } = lib.middlewares;
 
 const router = lib.Router();
 
-// // == DISCORD ==
-// // returns the OAuth URI
-// router.route(`/discord`)
-//   .get((req, res) => {
-//     const uri = config.discord.OAuthUri;
-//     if (uri) {
-//       return res.redirect(uri);
-//     }
-//     throw new ApiError(httpStatus.NOT_FOUND, `DISCORD_OAUTH_URI is not set`, true);
-//   });
+router.route(`/register`)
+  .post(
+    catchAsync(async (req, res) => {
+      const { username, password } = req.body;
+      const name = req.body.name ?? username;
 
-router.route(`/refresh`)
-  .get((req, res) => {
-  });
+      if (!username || !password) {
+        throw new ApiError(httpStatus.BAD_REQUEST, `username and password are required`);
+      }
+
+      let user = await services.user.getBy.username(username);
+      if (user) {
+        throw new ApiError(httpStatus.CONFLICT, `username already exists`);
+      }
+
+      const { salt, hash } = lib.util.crypto.generatePassword(password);
+
+      const newUser = await services.user.create({ name, salt, hash, username });
+
+      // const ip = req.headers[`x-forwarded-for`] || req.socket.remoteAddress;
+
+      const access_token = services.token.createAccessToken(newUser);
+      const refresh_token = await services.token.createRefreshToken(newUser);
+
+      res.json({
+        access_token,
+        refresh_token,
+        user: newUser.toJSON(),
+      });
+    }));
 
 // standard password and
 router.route(`/login`)
   .post(
-    Validate(lib.validations.auth.register),
     catchAsync(async (req, res) => {
       const { username, password } = req.body;
+
+      if (!username || !password) {
+        throw new ApiError(httpStatus.BAD_REQUEST, `username and password are required`);
+      }
 
       const user = await services.user.getBy.username(username);
       if (!user) {
@@ -44,33 +66,27 @@ router.route(`/login`)
         throw new ApiError(httpStatus.UNAUTHORIZED, `invalid password`);
       }
 
-      const ip = req.headers[`x-forwarded-for`] || req.socket.remoteAddress;
+      const access_token = services.token.createAccessToken(user);
+      const refresh_token = await services.token.createRefreshToken(user);
 
-      const tokenPair = await services.token.createSession(user, `session at ${ip}`);
-
-      res.json(tokenPair);
-
+      res.json({
+        access_token,
+        refresh_token,
+        user: user.toJSON(),
+      });
     }));
 
-router.route(`/register`)
+
+/**
+ * Protected route
+ */
+router.route(`/refresh`)
   .post(
-    Validate(lib.validations.auth.register),
-    catchAsync(async (req, res) => {
-      const { username, password } = req.body;
-
-      let user = await services.user.getBy.username(username);
-      if (user) {
-        throw new ApiError(httpStatus.CONFLICT, `username already exists`);
-      }
-
-      const { salt, hash } = lib.util.crypto.generatePassword(password);
-      const newUser = await services.user.create({ name: username, salt, hash, username: username });
-
-      const ip = req.headers[`x-forwarded-for`] || req.socket.remoteAddress;
-
-      const tokenPair = await services.token.createSession(newUser, `session at ${ip}`);
-
-      res.json(tokenPair);
-    }));
+    passport.authenticate(jwtRefreshStrategy),
+    (req, res) => {
+      res.json({
+        access_token: services.token.createAccessToken(req.user),
+      });
+    });
 
 export default router;
