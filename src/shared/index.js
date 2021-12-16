3683bb210b790fca80f95825dfb439d4f219743c
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 23:55:11 +0900

    comment out session

--- src/shared/index.js
+++ src/shared/index.js
@@ -5,7 +5,7 @@ import { RequireProxy } from "./RequireProxy.js";
 
 const mongo = new RequireProxy(() => connection.mongo.connection.isOperational(), {
   get User() { return connection.mongo.model(`User`, models.User); },
-  get Session() { return connection.mongo.model(`Session`, models.Session); },
+  // get Session() { return connection.mongo.model(`Session`, models.Session); },
   get Token() { return connection.mongo.model(`Token`, models.Token); },
   get Potd() { return connection.mongo.model(`Potd`, models.Potd); },
   get Data() { return connection.mongo.model(`Data`, models.Data, `data`); },
