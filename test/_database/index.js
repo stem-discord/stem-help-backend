0574a425c74cd71afabbdbdc0126bc42e5b26fed
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 23:54:15 +0900

    comment out sessions atm

--- test/_database/index.js
+++ test/_database/index.js
@@ -1,12 +1,12 @@
 
 import data from "./data.js";
-import sessions from "./sessions.js";
+// import sessions from "./sessions.js";
 import tokens from "./tokens.js";
 import users from "./users.js";
 
 export default {
   data,
-  sessions,
+  // sessions,
   tokens,
   users,
 };
