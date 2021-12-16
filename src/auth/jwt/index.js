e9358219d5a937ea1b00591c0ef946a7997ce803
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 23:54:37 +0900

    fix auth

--- src/auth/jwt/index.js
+++ src/auth/jwt/index.js
@@ -15,12 +15,12 @@ const jwtOptions = {
 /**
  * A passport authentication method
  */
-const jwtAuthenticate = passport.authenticate(async (payload, done) => {
+const jwtAccessStrategy = new JwtStrategy(jwtOptions, async (payload, done) => {
   if (payload.type !== Token.ACCESS) {
     return done(new Error(`Expected token type 'ACCESS'`), false);
   }
   try {
-    const user = await mongoose.model(`User`).findById(payload.id);
+    const user = await shared.mongo.User.findById(payload.sub);
     if (user) {
       return done(null, user);
     }
@@ -30,8 +30,28 @@ const jwtAuthenticate = passport.authenticate(async (payload, done) => {
   }
 });
 
+/**
+ * Extract from body refresh_token instead of header
+ */
+const jwtRefreshStrategy = new JwtStrategy({
+  ...jwtOptions,
+  jwtFromRequest: ExtractJwt.fromBodyField(`refresh_token`),
+}, async (payload, done) => {
+  if (payload.type !== Token.REFRESH) {
+    return done(new Error(`Expected token type 'REFRESH'`), false);
+  }
+  try {
+    const user = await shared.mongo.User.findById(payload.sub);
+    if (user) {
+      return done(null, user);
+    }
+    return done(null, false);
+  } catch (err) {
+    return done(err, false);
+  }
+});
 
-export { jwtAuthenticate };
+export { jwtAccessStrategy, jwtRefreshStrategy };
 
 // const jwtRefresh = async (payload, done) => {
 //   try {
