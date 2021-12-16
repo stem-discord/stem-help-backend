ab6b365e3f1d0bf2695fe81bb81c78d20e419ebb
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 23:55:04 +0900

    simplify token service

--- src/service/token.js
+++ src/service/token.js
@@ -1,8 +1,10 @@
 import jwt from "jsonwebtoken";
-import shared from "../shared/index.js";
 import config from "../config/index.js";
 import { Token as TokenType } from "../types/index.js";
 
+import { logger } from "../tool/index.js";
+
+import shared from "../shared/index.js";
 class Token {
   constructor(type, payload, expire) {
     if (!type || !payload || !expire) throw new Error(`Insufficient parameters`);
@@ -36,6 +38,7 @@ class AccessToken extends Token {
   constructor(user) {
     const payload = {
       sub: user._id,
+      user: user.toAccessToken(),
     };
     // TODO: softcode this
     super(TokenType.ACCESS, payload, `10m`);
@@ -43,67 +46,26 @@ class AccessToken extends Token {
 }
 
 class RefreshToken extends Token {
-  constructor(user, sessionId, jti) {
-    if (!sessionId || !jti) throw new Error(`Insufficient parameters`);
+  constructor(user, jti) {
+    if (!jti) throw new Error(`Insufficient parameters`);
     const payload = {
       sub: user._id,
       jti,
-      session: sessionId,
     };
     // TODO: softcode this
     super(TokenType.REFRESH, payload, `60days`);
   }
 }
 
-/**
- * Optionally return a refresh token as well
- */
-async function generateAccessToken(user, refreshToken) {
-  // TODO: review security
-  // redundant security checks
-  if (user._id !== refreshToken.sub) throw new Error(`subject is not identical to token subject`);
-  if (user.session !== refreshToken.session) throw new Error(`session is not the valid session`);
-
-  const accessToken = new AccessToken(user);
-  let newRefreshToken;
-
-  // FIXME: add duration checks
-  // eslint-disable-next-line
-  if (true) {
-    shared.mongo.Token.deleteOne({ _id: refreshToken._id });
-    const rt = await shared.mongo.Token.create();
-    refreshToken = new RefreshToken(user, user.session, rt._id).toString();
-  }
-
-  return {
-    access_token: accessToken,
-    refresh_token: newRefreshToken,
-  };
+function createAccessToken(user) {
+  return new AccessToken(user).toString();
 }
 
-/**
- * returns a pair, creates a new session in the database
- */
-async function createSession(user, sessionName) {
-  sessionName = sessionName ?? `default session`;
-  const session = await shared.mongo.Session.create({
-    name: sessionName,
+async function createRefreshToken(user) {
+  const token = await shared.mongo.Token.create({
+    user: user._id,
   });
-  const rt = await shared.mongo.Token.create({});
-  const refreshToken = new RefreshToken(user, session._id, rt._id).toString();
-  const accessToken = new AccessToken(user).toString();
-  return {
-    refresh_token: refreshToken,
-    access_token: accessToken,
-  };
+  return new RefreshToken(user, token._id).toString();
 }
 
-async function deleteSession(sessionId) {
-  return await shared.mongo.Session.deleteOne({ _id: sessionId });
-}
-
-async function deleteCurrentSession(user) {
-  return await Promise.all(deleteSession(user.session), shared.mongo.Token.deleteOne({ _id: user.jti }));
-}
-
-export { createSession, generateAccessToken, deleteSession, deleteCurrentSession };
+export { createAccessToken, createRefreshToken };
