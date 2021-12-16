945fa4e837e1620138110db4d556cb2baa596a01
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-16 23:53:31 +0900

    put under unit

--- test/unit/test.spec.js
+++ test/unit/test.spec.js
@@ -17,80 +17,101 @@ const {
 
 import * as validations from "../../src/validations/index.js";
 const { fields } = validations;
-describe(`pick.js`, function () {
-  it(`should work`, function () {
-    expect(pick({ a: 1, b: 2, c: 3 }, [`a`, `b`])).to.deep.equal({ a: 1, b: 2 });
-  });
-});
 
-describe(`DSA`, function () {
-  describe(`lcs.js`, function () {
-    const lcs = DSA.lcs;
-    it(`LCSFromStart`, function () {
-      const L = lcs.LCSFromStart;
-
-      expect(L(`a`)).to.equal(1);
-      expect(L(`ab`)).to.equal(2);
-      expect(L(`ab`, ``)).to.equal(0);
-      expect(L(`ab`, `abcde`)).to.equal(2);
-      expect(L(`ab`, `bcdefa`)).to.equal(0);
-      expect(L(`ab`, `abc`, `abcde`)).to.equal(2);
+describe(`util`, function () {
+  describe(`pick.js`, function () {
+    it(`should work`, function () {
+      expect(pick({ a: 1, b: 2, c: 3 }, [`a`, `b`])).to.deep.equal({ a: 1, b: 2 });
     });
   });
-});
 
-describe(`isMain`, function () {
-  it(`should work`, function () {
-    expect(() => isMain()).to.throw(/required/i);
-    expect(isMain(import.meta)).to.be.false;
+  describe(`DSA`, function () {
+    describe(`lcs.js`, function () {
+      const lcs = DSA.lcs;
+      it(`LCSFromStart`, function () {
+        const L = lcs.LCSFromStart;
+
+        expect(L(`a`)).to.equal(1);
+        expect(L(`ab`)).to.equal(2);
+        expect(L(`ab`, ``)).to.equal(0);
+        expect(L(`ab`, `abcde`)).to.equal(2);
+        expect(L(`ab`, `bcdefa`)).to.equal(0);
+        expect(L(`ab`, `abc`, `abcde`)).to.equal(2);
+      });
+    });
   });
-});
 
-describe(`normalize`, function () {
-  it(`should work`, function () {
-    expect(normalize(`Crème Brulée`)).to.equal(`creme brulee`);
+  describe(`isMain`, function () {
+    it(`should work`, function () {
+      expect(() => isMain()).to.throw(/required/i);
+      expect(isMain(import.meta)).to.be.false;
+    });
   });
-});
 
-describe(`catchAsync`, function () {
-  it(`should work`, async function () {
-    const fn = async () => {
-      throw new Error(`test`);
-    };
-    const func = chai.spy(() => {});
-    const err = await catchAsync(fn)(1, 1, func);
-    await expect(func).to.have.been.called();
+  describe(`normalize`, function () {
+    it(`should work`, function () {
+      expect(normalize(`Crème Brulée`)).to.equal(`creme brulee`);
+    });
   });
-});
 
-describe(`nullWrapper`, function () {
-  it(`should work`, function () {
-    expect(nullWrapper(() => {
-      throw Error(`test`);
-    })).to.equal(null);
-    expect(nullWrapper(() => {
-      return 1;
-    })).to.equal(1);
-  });
-});
-
-describe(`async`, function () {
-  describe(`sleep.js`, function () {
+  describe(`catchAsync`, function () {
     it(`should work`, async function () {
-      const start = Date.now();
-      await async.sleep(2);
-      const end = Date.now();
-      expect(end - start).to.be.least(1);
+      const fn = async () => {
+        throw new Error(`test`);
+      };
+      const func = chai.spy(() => {});
+      const err = await catchAsync(fn)(1, 1, func);
+      await expect(func).to.have.been.called();
     });
   });
+
+  describe(`nullWrapper`, function () {
+    it(`should work`, function () {
+      expect(nullWrapper(() => {
+        throw Error(`test`);
+      })).to.equal(null);
+      expect(nullWrapper(() => {
+        return 1;
+      })).to.equal(1);
+    });
+  });
+
+  describe(`async`, function () {
+    describe(`sleep.js`, function () {
+      it(`should work`, async function () {
+        const start = Date.now();
+        await async.sleep(2);
+        const end = Date.now();
+        expect(end - start).to.be.least(1);
+      });
+    });
+  });
+
+  describe(`randomIdentifier.js`, function () {
+    it(`should work`, function () {
+      expect(randomIdentifier()).to.be.a(`string`);
+      const arr = Array.from({ length: 4 }, randomIdentifier);
+      // length should be the same, or it is not unique
+      expect(arr.length).to.equal(new Set(arr).size);
+    });
+  });
+
 });
 
-describe(`randomIdentifier.js`, function () {
-  it(`should work`, function () {
-    expect(randomIdentifier()).to.be.a(`string`);
-    const arr = Array.from({ length: 4 }, randomIdentifier);
-    // length should be the same, or it is not unique
-    expect(arr.length).to.equal(new Set(arr).size);
+function validationFactory(fieldname, passing, failing, validator) {
+  describe(`validation for ${fieldname}`, function () {
+    it(`Should pass passing usernames`, function () {
+      for (const pass of passing) {
+        expect(Joi.string().custom(validator).validate(pass), `expected '${pass}' to be a valid ${fieldname}`).to.not.have.property(`error`);
+      }
+    });
+    it(`Should not pass invalid usernames`, function () {
+      for (const fail of failing) {
+        expect(Joi.string().custom(validator).validate(fail), `expected '${fail}' to not be a valid ${fieldname}`).to.have.property(`error`);
+      }
+    });
+  });
+}
 
 describe(`validations`, function () {
   describe(`field`, function () {
