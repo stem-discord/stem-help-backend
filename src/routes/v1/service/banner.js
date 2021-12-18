ef42f854f3aec2fd395e27e6d1feb238a6058d4b
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-18 17:39:36 +0900

    use blazing fast cache for banners

--- src/routes/v1/service/banner.js
+++ src/routes/v1/service/banner.js
@@ -5,12 +5,27 @@ const config = lib.config;
 
 const router = lib.Router();
 
+const canvasCache = new lib.util.cache.NodeCache({
+  stdTTL: 600,
+  checkperiod: 60,
+  useClones: false,
+});
+
+const htmlCache = new lib.util.cache.NodeCache({
+  stdTTL: 600,
+  checkperiod: 60,
+  useClones: false,
+});
+
 router.get(
   `/html/*`,
   catchAsync(async (req, res) => {
     const text = req.params[0].replace(/_/g, ` `);
 
-    const buf = await lib.service.generatePngFromHtml.generate(text);
+    const buf =
+      htmlCache.get(text) ??
+      (await lib.service.generatePngFromHtml.generate(text));
+    htmlCache.set(text, buf);
 
     res.writeHead(200, {
       "Content-Type": `image/png`,
@@ -26,7 +41,10 @@ router.get(
   catchAsync(async (req, res) => {
     const text = req.params[0].replace(/_/g, ` `);
 
-    const buf = await lib.service.generatePngBanner.generate(text);
+    const buf =
+      canvasCache.get(text) ??
+      (await lib.service.generatePngBanner.generate(text));
+    canvasCache.set(text, buf);
 
     res.writeHead(200, {
       "Content-Type": `image/png`,
