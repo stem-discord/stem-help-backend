7d2b9f20a0f9fa9af6bfb1e338e57c621a21a3f9
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-21 06:09:17 +0900

    add badge system

---
+++ src/routes/v1/service/badge.js
@@ -0,0 +1,42 @@
+import * as lib from "../../lib/index.js";
+import fetch from "node-fetch";
+
+const { ApiError, catchAsync, pick, dirname, cache, git } = lib.util;
+const config = lib.config;
+
+const router = lib.Router();
+
+let badgeHtml;
+
+const stats = {};
+
+// TODO idk make this customizable
+function builder(title = ``, description = ``, color = `ff69b4`, logo = ``) {
+  return `https://img.shields.io/badge/${encodeURI(title)}-${encodeURI(
+    description
+  )}-${encodeURI(color)}?style=for-the-badge&logo=${encodeURI(logo)}`;
+}
+
+router.get(
+  `/git/:query`,
+  catchAsync(async (req, res) => {
+    const query = req.params.query;
+
+    if (query === `file-count`) {
+      stats.fileCount ??= git.status
+        .getFileCount()
+        .then(v => fetch(builder(`File Count`, v, undefined, `git`)))
+        .then(v => v.text());
+      // eslint-disable-next-line require-atomic-updates
+      stats.fileCount = await stats.fileCount;
+
+      return res
+        .setHeader(`Content-Type`, `image/svg+xml`)
+        .send(stats.fileCount);
+    }
+
+    throw new ApiError(404, `Query is not valid`);
+  })
+);
+
+export default router;
