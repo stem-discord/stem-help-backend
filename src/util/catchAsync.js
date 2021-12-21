0aeb64f84a7b66fce6fa35b3053b0931b48cad25
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-21 19:22:07 +0900

    return the promise for testing

--- src/util/catchAsync.js
+++ src/util/catchAsync.js
@@ -1,5 +1,4 @@
-const catchAsync = fn => (req, res, next) => {
+const catchAsync = fn => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(err => next(err));
-};
 
 export default catchAsync;
