1a6793edec415d38e25dda516b30f82e9d4b8225
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-28 20:21:01 +0900

    set minimum limit to 20

--- src/validations/discord/oauth.js
+++ src/validations/discord/oauth.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const token = (value, helpers) => {
   // TODO: I'm not sure if this is the correct regex, but its from observation
-  if (!value.match(/^[\da-zA-Z_-]+$/)) {
+  if (!value.match(/^[\da-zA-Z_-]{20,}$/)) {
     return helpers.message(`"{{#label}}" must be a valid token`);
   }
   return value;
