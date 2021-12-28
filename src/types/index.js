bae0412979f99bd3b2a957fc3ae2ec39736d0e45
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-28 19:29:03 +0900

    export lock

--- src/types/index.js
+++ src/types/index.js
@@ -3,3 +3,4 @@ export { default as Role } from "./Role/index.js";
 export { default as Group } from "./Group/index.js";
 export { default as ConnectionEvent } from "./ConnectionEvent/index.js";
 export { default as ConnectionState } from "./ConnectionState/index.js";
+export { default as Lock } from "./Lock.js";
