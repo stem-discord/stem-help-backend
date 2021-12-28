0bceb742fbd546694dab4cba21384ef52a1aa77a
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-28 19:10:41 +0900

    optimize validation middleware factory

--- src/middlewares/Validate.js
+++ src/middlewares/Validate.js
@@ -1,23 +1,38 @@
 import Joi from "joi";
 import httpStatus from "http-status";
+import util from "util";
 
 import { ApiError, pick } from "../util/index.js";
 
-const validate = schema => (req, res, next) => {
+const validate = schema => {
   const validSchema = pick(schema, [`params`, `query`, `body`]);
-  const object = pick(req, Object.keys(validSchema));
-  const { value, error } = Joi.compile(validSchema)
-    .prefs({ errors: { label: `key` }, abortEarly: false })
-    .validate(object);
+  const keys = Object.keys(validSchema);
 
-  if (error) {
-    const errorMessage = error.details
-      .map(details => details.message)
-      .join(`, `);
-    return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+  if (keys.length === 0) {
+    throw new Error(
+      `Schema was empty or didn't have params, query, body, ${util.format(
+        schema
+      )}`
+    );
   }
-  Object.assign(req, value);
-  return next();
+
+  const comp = Joi.compile(validSchema);
+
+  return (req, res, next) => {
+    const object = pick(req, keys);
+    const { value, error } = comp
+      .prefs({ errors: { label: `key` }, abortEarly: false })
+      .validate(object);
+
+    if (error) {
+      const errorMessage = error.details
+        .map(details => details.message)
+        .join(`, `);
+      return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+    }
+    Object.assign(req, value);
+    return next();
+  };
 };
 
 export default validate;
