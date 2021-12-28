21809d5557ce87c2b4e885423725dca23274e25d
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-28 18:39:07 +0900

    wrap it in api endpoints test

--- test/middlewares/main.spec.js
+++ test/middlewares/main.spec.js
@@ -3,24 +3,26 @@ import { discord } from "../../src/service/index.js";
 
 import * as wrapper from "./wrapper.js";
 
-describe(`v1`, function () {
-  describe(`events`, function () {
-    describe(`christmastree`, function () {
-      describe(`token validations`, function () {
-        const v = v1.events.tokenCheck;
-        it(`should invalidate`, async function () {
-          await wrapper.middleware(v).next(/missing|no/i);
-          await wrapper
-            .middleware(v, { body: { token: `1234` } })
-            .next(/format/i);
-          await wrapper
-            .middleware(v, { body: { token: `1234_1234` } })
-            .next(/invalid/i);
-        });
-        it(`should validate`, async function () {
-          // Register cache
-          const token = discord.createToken(`12345678`);
-          await wrapper.middleware(v, { body: { token } });
+describe(`API endpoint controllers`, function () {
+  describe(`v1`, function () {
+    describe(`events`, function () {
+      describe(`christmastree`, function () {
+        describe(`token validations`, function () {
+          const v = v1.events.tokenCheck;
+          it(`should invalidate`, async function () {
+            await wrapper.middleware(v).next(/missing|no/i);
+            await wrapper
+              .middleware(v, { body: { token: `1234` } })
+              .next(/format/i);
+            await wrapper
+              .middleware(v, { body: { token: `1234_1234` } })
+              .next(/invalid/i);
+          });
+          it(`should validate`, async function () {
+            // Register cache
+            const token = discord.createToken(`12345678`);
+            await wrapper.middleware(v, { body: { token } });
+          });
         });
       });
     });
