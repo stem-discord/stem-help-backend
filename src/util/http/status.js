3e3e35229169311176270b02771f7187f39eea59
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-28 20:27:29 +0900

    undefined checks

--- src/util/http/status.js
+++ src/util/http/status.js
@@ -1,10 +1,16 @@
 import httpStatus from "http-status";
 
 function isOperational(status) {
+  if (status === undefined) {
+    throw new Error(`status is required`);
+  }
   return status >= 200 && status < 300;
 }
 
 function getDescription(status) {
+  if (status === undefined) {
+    throw new Error(`status is required`);
+  }
   const s = httpStatus[status];
   if (!s) throw new Error(`Recieved invalid status ${status}`);
   return s;
