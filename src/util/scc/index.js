42dc270d02c02a0c919f677504e5fae895fa1a59
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-28 20:52:57 +0900

    show that scc is not available if it is not installed

--- src/util/scc/index.js
+++ src/util/scc/index.js
@@ -1,6 +1,10 @@
 import { exec } from "child_process";
+import { sync } from "command-exists";
+
+const isSccInstalled = sync(`scc`);
 
 export async function stats() {
+  if (!isSccInstalled) return `<SCC information not available>`;
   return new Promise((res, rej) => {
     exec(`scc --exclude-dir package-lock.json`, (err, stdout, stderr) => {
       if (err || stderr) {
