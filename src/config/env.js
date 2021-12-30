e5ba2aeca9592a5af668b31bf4074d79793fe9ed
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-30 20:56:33 +0900

    fix splitting

--- src/config/env.js
+++ src/config/env.js
@@ -18,7 +18,7 @@ function generateConfig(env) {
     port: envVars.PORT,
     apiURL: envVars.API_URL,
     adminKey: envVars.SUPER_SECRET_ADMIN_KEY,
-    connections: envVars.CONNECTIONS?.split(`,`),
+    connections: envVars.CONNECTIONS?.split(/\s*,\s*/g),
     logging: {
       absolute: bool(envVars.LOGGING_ABSOLUTE),
     },
