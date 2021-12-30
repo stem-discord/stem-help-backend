0c5b820a58f35cdfa75f0aa9d5c425fd1de41c42
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-30 21:09:43 +0900

    add simple cache

--- src/util/scc/index.js
+++ src/util/scc/index.js
@@ -3,14 +3,19 @@ import { sync } from "command-exists";
 
 const isSccInstalled = sync(`scc`);
 
+let sccCache;
+
 export async function stats() {
   if (!isSccInstalled) return `<SCC information not available>`;
-  return new Promise((res, rej) => {
-    exec(`scc --exclude-dir package-lock.json`, (err, stdout, stderr) => {
-      if (err || stderr) {
-        rej(err || stderr);
-      }
-      res(stdout);
-    });
-  });
+  return (
+    sccCache ??
+    (sccCache = new Promise((res, rej) => {
+      exec(`scc --exclude-dir package-lock.json`, (err, stdout, stderr) => {
+        if (err || stderr) {
+          rej(err || stderr);
+        }
+        res(stdout);
+      });
+    }))
+  );
 }
