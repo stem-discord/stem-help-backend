f349881cdcb7d45d5daa80758f2ca6d8a39a7bdb
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-30 21:03:46 +0900

    stem route

--- src/routes/v1/service/index.js
+++ src/routes/v1/service/index.js
@@ -3,6 +3,7 @@ import discordidlookup from "./discordidlookup.js";
 import banner from "./banner.js";
 import badge from "./badge.js";
 import dependency from "./dependency.js";
+import stem from "./stem.js";
 
 import * as lib from "../../lib/index.js";
 
@@ -17,5 +18,7 @@ router.use(`/banner`, banner);
 router.use(`/badge`, badge);
 
 router.use(`/dependency`, dependency);
+
+router.use(`/stem`, stem);
 
 export default router;
---
+++ src/routes/v1/service/stem.js
@@ -0,0 +1,24 @@
+import httpStatus from "http-status";
+
+import * as lib from "../../lib/index.js";
+
+const { ApiError, catchAsync } = lib.util;
+
+const router = lib.Router();
+
+router.get(
+  `/thankedstats/:query?`,
+  catchAsync(async (req, res) => {
+    const query = req.params.query;
+    if (!query) {
+      throw new ApiError(httpStatus.BAD_REQUEST, `Missing query`);
+    }
+
+    // Will return false if user is not found
+    const r = await lib.service.stemInformation.getUserStats(query);
+
+    res.json(r);
+  })
+);
+
+export default router;
