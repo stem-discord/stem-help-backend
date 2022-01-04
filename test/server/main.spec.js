2feed466953a10ce0f840be5dde97073625e0f13
nopeless <38830903+nopeless@users.noreply.github.com>
2022-01-05 04:54:12 +0900

    implement nock in real test

--- test/server/main.spec.js
+++ test/server/main.spec.js
@@ -2,18 +2,21 @@ import fetch from "node-fetch";
 import { nock } from "../mock/index.js";
 
 describe(`discord api`, function () {
-  it(`should return 404 (mock)`, async function () {
-    nock(`discord.com`);
-    const res = await fetch(`https://discord.com/api/v9/`).then(v => v.json());
-    expect(res).to.have.property(`message`);
-    expect(res).to.have.property(`custom`);
-  });
-
   it(`should return 404 (real)`, async function () {
-    this.skip();
     const res = await fetch(`https://discord.com/api/v9/`).then(v => v.json());
     expect(res).to.have.property(`message`);
     expect(res).to.not.have.property(`custom`);
+  });
+
+  describe(`nock`, function () {
+    nock(`discord.com`);
+    it(`should return 404 (mock)`, async function () {
+      const res = await fetch(`https://discord.com/api/v9/`).then(v =>
+        v.json()
+      );
+      expect(res).to.have.property(`message`);
+      expect(res).to.have.property(`custom`);
+    });
   });
 });
 
