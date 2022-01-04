82d95fe1e03ee0c760f255c379b0e612c743ffef
nopeless <38830903+nopeless@users.noreply.github.com>
2022-01-05 03:50:51 +0900

    optimize dependency graph generation for jpg/jpeg

--- src/service/generateDependencyGraph.js
+++ src/service/generateDependencyGraph.js
@@ -5,6 +5,8 @@ import { ApiError } from "../util/index.js";
 import { promisify } from "util";
 import svg2img from "svg2img";
 
+import Jimp from "jimp";
+
 const isGraphVizInstalled = sync(`gvpr`);
 
 const validTypes = [`svg`, `png`];
@@ -12,6 +14,20 @@ const validTypes = [`svg`, `png`];
 const m = madge(`src/server.js`, {
   includeNpm: true,
 });
+
+async function optimize(p, { type = `image/png`, width = 2000 }) {
+  return p.then(v =>
+    Jimp.read(v)
+      .then(lenna => lenna.resize(width, Jimp.AUTO).getBufferAsync(type))
+      // TODO remove this catch clause after Jimp fixes https://github.com/oliver-moran/jimp/issues/1057
+      .catch(e => {
+        if (e instanceof Error) {
+          throw e;
+        }
+        throw new Error(e);
+      })
+  );
+}
 
 async function generate(type = `svg`) {
   if (!isGraphVizInstalled)
@@ -21,9 +37,13 @@ async function generate(type = `svg`) {
 
   if (type === `png`) {
     const svg = await generate(`svg`);
-    return promisify(svg2img.bind(svg2img))(svg);
+    return optimize(promisify(svg2img.bind(svg2img))(svg));
+  } else if ([`jpeg`, `jpg`].includes(type)) {
+    const svg = await generate(`svg`);
+    return optimize(promisify(svg2img.bind(svg2img))(svg), {
+      type: `image/jpeg`,
+    });
   }
-
   return m.then(res => res[type]());
 }
 
