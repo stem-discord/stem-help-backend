9f842c2acff51b4d777e71451d6f3313af7ee07f
nopeless <38830903+nopeless@users.noreply.github.com>
2022-01-13 00:48:34 +0900

    revamp api system

--- src/routes/v1/status/index.js
+++ src/routes/v1/status/index.js
@@ -1,18 +1,121 @@
+import httpStatus from "http-status";
+import fetch from "node-fetch";
+
 import * as lib from "../../lib/index.js";
 
 import { connections } from "../../../connection/index.js";
 
+const { logger } = lib.tool;
+const { catchAsync } = lib.util;
+const { ConnectionState } = lib.types;
+
 const router = lib.Router();
 
+function createConnection(
+  name = `Unknown`,
+  state = ConnectionState.UNKNOWN,
+  operational,
+  meta = {}
+) {
+  if (!ConnectionState[state]) {
+    logger.error(`Invalid connection state: ${state}`);
+    state = ConnectionState.INVALID;
+  }
+  operational ??= state === ConnectionState.CONNECTED;
+  return {
+    name,
+    state,
+    operational,
+    meta,
+  };
+}
+
+function createNamespacedApiStatus(id, status, connections) {
+  if (!Array.isArray(connections)) {
+    throw new Error(`connections must be an array`);
+  }
+
+  if (!httpStatus[status]) {
+    status = httpStatus[500];
+    logger.error(`Invalid status: ${status}`);
+  }
+
+  // Should always return string codes
+  status = Number.isInteger(status) ? httpStatus[status] : status;
+
+  return {
+    [id]: {
+      status,
+      connections,
+    },
+  };
+}
+
+function getLocalStatus() {
+  return createNamespacedApiStatus(
+    `STEM_HELP_API_SERVER`,
+    `OK`,
+    connections.map(c => createConnection(c.name, c.state, c.isOperational()))
+  );
+}
+
 router.get(`/`, (req, res) => {
+  res.json(getLocalStatus());
+});
+
+/**
+ * meta route to return data
+ */
+router.get(`/meta`, (req, res) => {
   res.json({
-    status: `ok`,
-    connections: connections.map(connection => ({
-      name: connection.name,
-      state: connection.state,
-      operational: connection.isOperational(),
-    })),
+    metadata: {
+      STEM_HELP_API_SERVER: {
+        description: `STEM Help API Server that is a gateway for all API`,
+        version: `1.0.0`,
+      },
+      STEM_SHEILD: {
+        description: `A service that helps reduce spam and abuse in the community by verifying new accounts`,
+        version: `1.0.0`,
+      },
+    },
   });
 });
+
+router.get(
+  `/everything`,
+  catchAsync(async (req, res) => {
+    res.json({
+      ...getLocalStatus(),
+      ...createNamespacedApiStatus(
+        `STEM_SHIELD`,
+        ...(await (async () => {
+          const { data } = await fetch(`https://mod.stem.help/api/status`).then(
+            v => v.json()
+          );
+
+          if (data === undefined) throw new Error(`No data`);
+
+          return [
+            httpStatus.OK,
+            data.map(v =>
+              createConnection(
+                v.name,
+                {
+                  online: ConnectionState.CONNECTED,
+                  offline: ConnectionState.DISCONNECTED,
+                }[v.status],
+                v.status === `online`,
+                v
+              )
+            ),
+          ];
+        })().catch(e => {
+          logger.error(e);
+          return [httpStatus.INTERNAL_SERVER_ERROR, []];
+        }))
+      ),
+    });
+  })
+);
 
 export default router;
