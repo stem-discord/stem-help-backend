5706c853bfbbb671578b3130769d7c6cf427514e
nopeless <38830903+nopeless@users.noreply.github.com>
2022-04-13 17:24:31 +0900

    fix testenv script

--- script/testenv.js
+++ script/testenv.js
@@ -6,9 +6,17 @@ const file = dirname(import.meta, `../test/.env`);
 
 if (!fs.existsSync(file)) {
   // TODO softcode this
-  // If you have a lot of time, just rewrite the entire setup thing
-  let envVars = envVarsSchema.validate({}).value;
-  if (!envVars.PORT) throw new Error(`Pass in a PORT environment variable!`);
-  fs.writeFileSync(file, `API_URL=http://localhost:${envVars.PORT}/v1`);
+  let PORT = process.env.PORT;
+
+  if (!PORT) {
+    // If you have a lot of time, just rewrite the entire setup thing
+    let envVars = envVarsSchema.validate({}).value;
+    if (!envVars.PORT)
+      throw new Error(
+        `Pass in a PORT environment variable or set a default value for port in env vars schema`
+      );
+  }
+
+  fs.writeFileSync(file, `API_URL=http://localhost:${PORT}/v1`);
   console.log(`./test/.env file did not exist. generated one`);
 }
