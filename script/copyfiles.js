e22534234ab8a3b00e3520f3ad360a9c69c42278
nopeless <38830903+nopeless@users.noreply.github.com>
2022-04-24 02:26:41 +0900

    use my copyfiles instead cuz npm devs suck

---
+++ script/copyfiles.js
@@ -0,0 +1,58 @@
+// Stupid npm developers don't even know how to write
+// a copy file script without it having to have millions o f stupid
+// pieces of garbage in their code like bruh its copying frikin file how complicated can it frikin be
+// you don't even know how to write a copy file script without it having to have millions of stupid
+
+import fs from "fs";
+import path from "path";
+
+const cwd = process.cwd();
+
+console.log(`working on ${cwd}`);
+
+function* walk(dir) {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      yield* walk(filePath);
+    } else {
+      yield filePath;
+    }
+  }
+}
+
+function apply({
+  validator = _file => true,
+  source,
+  destination,
+  destinationTransformer = null,
+  overwrite = false,
+}) {
+  if (!source) throw new Error(`source is required`);
+  if (!destination && !destinationTransformer)
+    throw new Error(`destination or destinationTransformer is required`);
+
+  for (const file of walk(source)) {
+    if (validator(file)) {
+      const destinationFile = destinationTransformer
+        ? destinationTransformer(file)
+        : path.join(destination, path.relative(source, file));
+      if (fs.existsSync(destinationFile) && !overwrite) {
+        console.debug(`skipping ${file}`);
+        continue;
+      }
+      fs.mkdirSync(path.dirname(destinationFile), { recursive: true });
+      fs.copyFileSync(file, destinationFile);
+    }
+  }
+
+  return true;
+}
+
+apply({
+  source: `src`,
+  destination: `dist`,
+  validator: file => !(file.endsWith(`.js`) || file.endsWith(`.ts`)),
+});
