3354c8ffade6640921b84c452dc720e02884bc00
nopeless <38830903+nopeless@users.noreply.github.com>
2022-04-25 06:05:53 +0900

    use any to make tsc happy

--- src/service/stemInformation.ts
+++ src/service/stemInformation.ts
@@ -6,12 +6,15 @@ import config from "../config/index.js";
  */
 export async function getUserStats(id: string) {
   const query = { server: config.discord.server.stem, user_id: id };
-  const q = await shared.stemInformation.UserInfoPublic.findOne(query).lean();
+  const q = (await shared.stemInformation.UserInfoPublic.findOne(
+    query
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ).lean()) as any;
   const q2 = await shared.stemInformation.ThankedLog.findOne(query).lean();
   if (!q) return false;
   if (!q2) return false;
   return {
     ...q2,
-    stats: q.stats,
+    stats: q?.stats,
   };
 }
