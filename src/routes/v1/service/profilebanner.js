c9eef5a2e4f0c0fdd07176fa07a2be4760bfaf74
nopeless <38830903+nopeless@users.noreply.github.com>
2022-04-25 03:48:09 +0900

    remove console log

--- src/routes/v1/service/profilebanner.js
+++ src/routes/v1/service/profilebanner.js
@@ -27,8 +27,6 @@ const profileBannerCache = new lib.util.cache.FileSystemCache({
         .query`query { user(id: "${id}") { displayAvatarURL(format: "png", size: 128) } }`
     ).data;
 
-    console.log(user);
-
     let pfp =
       user.displayAvatarURL ||
       `https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png`;
