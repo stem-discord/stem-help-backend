fa606e5134b7ef7c473be7643246f95a058f5aa1
nopeless <38830903+nopeless@users.noreply.github.com>
2022-04-28 19:07:37 +0900

    remove useless code

--- src/discord-bot.ts
+++ src/discord-bot.ts
@@ -1,8 +1,6 @@
-import dotenv from "dotenv";
-
 import { openConnections, discord } from "./connection/index.js";
 
-openConnections([`discord`, `mongo`]);
+openConnections([`discord`]);
 
 const { client } = discord;
 
