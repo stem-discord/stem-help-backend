3f6aeca1068736dd64a9f5d2e68772974d553052
nopeless <38830903+nopeless@users.noreply.github.com>
2022-05-06 02:02:16 +0900

    add xpiowolf to database

--- test/_database/users.js
+++ test/_database/users.js
@@ -12,4 +12,11 @@ export default [
     hash,
     roles: [`ADMINISTRATOR`],
   },
+  {
+    name: `Xpiowolf`,
+    username: `xpiowolf`,
+    salt: `25d33dcf27d213c52e02b948b5e586c5c99c8f89abfae4f5ab21ec9cfff1ef97`,
+    hash: `31b30955e2df8abb2a1b3ac13c1663a76f1bb0164dc60ea8840b3e26553cb0685c3656068a7085978f01c4cf9069191543e46e7edd68dc935028672bcee5e818`,
+    roles: [`ADMINISTRATOR`],
+  },
 ];
