b5143e37026ab450dad2d79407a632342d46f8ce
nopeless <38830903+nopeless@users.noreply.github.com>
2022-05-18 00:18:05 +0900

    remove auth related things

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,6 @@
 export { default as Session } from "./session.js";
 export { default as User } from "./user.js";
 export { default as Potd } from "./potd.js";
-export { default as Token } from "./token.js";
 export { default as Data } from "./data.js";
 export { default as AnySchema } from "./anySchema.js";
 export { default as Error } from "./error.js";
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,4 @@
 import docs from "./docs/index.js";
-import auth from "./auth/index.js";
-import oauth from "./oauth/index.js";
 import data from "./data/index.js";
 import service from "./service/index.js";
 import status from "./status/index.js";
@@ -11,10 +9,6 @@ import * as lib from "../lib/index.js";
 const router = new lib.Router();
 
 router.use(`/docs`, docs);
-
-router.use(`/auth`, auth);
-
-router.use(`/oauth`, oauth);
 
 router.use(`/data`, data);
 
@@ -27,8 +21,6 @@ router.use(`/events`, events);
 export default router;
 
 export * as docs from "./docs/index.js";
-export * as auth from "./auth/index.js";
-export * as oauth from "./oauth/index.js";
 export * as data from "./data/index.js";
 export * as service from "./service/index.js";
 export * as status from "./status/index.js";
